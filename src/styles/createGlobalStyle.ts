ada62385b5bc3366c38a517f795e7cac7f5af30f
Rafael Dias <rafael.dias@evolutionsoft.com.br>
2023-03-20 19:16:24 -0300

    Criando estilos globais

---
+++ src/styles/createGlobalStyle.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+import { DefaultThemeProps } from "../types/themeTypes";
+
+export const GlobalStyle = createGlobalStyle<{theme: DefaultThemeProps}>`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: ${props => props.theme.fontFamily};
+  }
+  body {
+    background: ${props => props.theme.backgroundColor};
+  }
+`;
---
+++ src/types/themeTypes.ts
@@ -0,0 +1,21 @@
+export interface DefaultThemeProps {
+  spacing: {
+		small: string;
+		medium: string;
+		large: string;
+	},
+	colors: {
+		primary: string;
+		secondaryDark: string;
+		secondaryLight: string;
+		greyPrimary: string;
+		greySecondary: string;
+		greyMid: string;
+		greyLight: string;
+		iceWhite: string;
+		white: string;
+	},
+	fontFamily: string;
+	borderRadius: string;
+  backgroundColor: string;
+}
