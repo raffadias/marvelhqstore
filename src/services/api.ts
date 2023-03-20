7efe506edf6185c712ed06ef4067cd15592f2f9d
Rafael Dias <rafael.dias@evolutionsoft.com.br>
2023-03-20 18:26:25 -0300

    Configurando ambiente, axios e chaves da API

---
+++ src/services/api.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+import md5 from "md5";
+
+const baseURL = import.meta.env.VITE_URL;
+const publicKey = import.meta.env.VITE_PUBLIC_KEY;
+const privateKey = import.meta.env.VITE_PRIVATE_KEY;
+const ts = Number(new Date());
+
+const hash = md5(ts + privateKey + publicKey);
+
+const api = axios.create({
+    baseURL: baseURL,
+    params: {
+        ts,
+        apikey: publicKey,
+        hash
+    }
+});
+
+export default api;
---
+++ src/services/comics.ts
@@ -0,0 +1,5 @@
+import api from "./api";
+
+export async function getComicBooks() {
+    return await api.get("/comics");
+}
